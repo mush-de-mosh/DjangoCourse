48e151043af155daa50fab85fd1b764355a741f6
Moses Mushabe <mushdemosh143@gmail.com>
2025-12-26 08:54:45 +0300

    Add a Movie and Delete it

--- movies/urls.py
+++ movies/urls.py
@@ -17,7 +17,14 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
+from movies import views
 
 urlpatterns = [
+    path ('', views.home),
     path("admin/", admin.site.urls),
+    path ("movies/", views.movies),
+    path ("movielist/", views.movielist),
+    path ("movielist/<int:id>", views.detail),
+    path ("movies/add", views.add),
+    path ("movies/delete/<int:id>", views.delete)
 ]
---
+++ movies/views.py
@@ -0,0 +1,36 @@
+from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.shortcuts import render
+from .models import Movie
+
+def movies(request):
+    return HttpResponse("Hello there!")
+
+
+def movielist(request):
+    data = Movie.objects.all()
+    return render(request, "movies/movies.html",{"movies": data})
+
+def home(request):
+    return HttpResponse("Welcome to my new application")
+
+def detail(request, id):
+    data = Movie.objects.get(pk = id)
+    return render(request, "movies/detail.html", {"movies": data})
+
+def add(request):
+    title = request.POST.get("title")
+    year = request.POST.get("year")
+    
+    if title and year:
+        movie = Movie(title=title, year=year)
+        movie.save()
+        return HttpResponseRedirect("/movielist/")
+    return render(request, "movies/add.html")
+
+def delete(request, id):
+    try:
+        movie = Movie.objects.get(pk=id)
+    except Movie.DoesNotExist:
+        raise Http404("Movie does not exist")
+    movie.delete()
+    return HttpResponseRedirect("/movielist/")
